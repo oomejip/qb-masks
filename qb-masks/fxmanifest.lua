883cf038ddf3d6c3034dddaa8d5a9f901af22980
OOEMJIP <69800408+oomejip@users.noreply.github.com>
2022-09-17 20:54:19 +0100

    Update fxmanifest.lua

--- qb-masks/fxmanifest.lua
+++ qb-masks/fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'qb-masks '
-version '1.0.0'
+description 'qb-masks-helmets '
+version '1.0.1'
 
 client_script 'client.lua'
 server_script 'server.lua'
