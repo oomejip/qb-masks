cb50e23ce0617f9d6b8692db0d68f658e7bdce71
OOEMJIP <69800408+oomejip@users.noreply.github.com>
2022-09-17 20:54:42 +0100

    Update server.lua

--- qb-masks/server.lua
+++ qb-masks/server.lua
@@ -40,3 +40,137 @@ QBCore.Functions.CreateUseableItem("skullmask", function(source, item)
     local Player = QBCore.Functions.GetPlayer(src)
     TriggerClientEvent("masks:client:skullmask", src, item.name)
 end)
+
+QBCore.Functions.CreateUseableItem("terrorwit", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:terrorwit", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("feest", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:feest", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("noselong", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:noselong", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("skullzwart", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:skullzwart", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("silver", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:silver", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("zwart1", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:zwart1", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("zwart2", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:zwart1", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("please", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:please", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("sok", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:sok", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("zak", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:zak", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("groen", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:groen", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("bandanab", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:bandanab", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("bivak1", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:bivak1", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("roodc", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:roodc", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("clown", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:clown", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("clown", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:clown", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("gorilla", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:gorilla", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("paard", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:paard", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("paardu", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:paardu", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("skullr", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:skullr", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("masker3", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:masker3", src, item.name)
+end)
+
+QBCore.Functions.CreateUseableItem("helm1", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("masks:client:helm1", src, item.name)
+end)
+
+
